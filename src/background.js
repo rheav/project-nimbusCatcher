6f69183d5b3be5572806cb28701fc57dd03ee1e0
rheav <rheav7@gmail.com>
2024-02-27 16:12:05 -0300

    Your commit message

--- src/background.js
+++ src/background.js
@@ -60,6 +60,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 // Listener for when a tab is removed (closed)
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  // Clear the flag when the tab is closed
+  // Clear the flag when the tab is closeda
   chrome.storage.local.remove([`scriptRun_${tabId}`]);
 });
